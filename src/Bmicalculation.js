f7ada5548235d61c572be20acc82afd7e5c0a772
Abhishek Ambure <54985327+abhishekambure@users.noreply.github.com>
2021-12-11 16:23:38 +0530

    Update Bmicalculation.js

--- src/Bmicalculation.js
+++ src/Bmicalculation.js
@@ -44,7 +44,7 @@ function Bmicalculation() {
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 
         <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">Enter Height in Meters</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">Enter Height in cm</label>
             <input className="shadow appearance-none border rounded w-100px py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Height" type="text" placeholder="Height" value={height} onChange={event => setHeight(event.target.value)}/>
             </div>
 
